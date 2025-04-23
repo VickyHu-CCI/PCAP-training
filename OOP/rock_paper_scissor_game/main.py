4396c1d49f356087967ae0167af60423a8bc9494
vicky hu <vicky.hu@clearchannelint.com>
2025-04-23 17:58:31 +0100

    Refactor try-except block

--- OOP/rock_paper_scissor_game/main.py
+++ OOP/rock_paper_scissor_game/main.py
@@ -5,13 +5,23 @@ from models.game import Game
 
 def main():
     print('--- Rock Paper Scissors Game ---')
+    # while True:
+    #     number_of_rounds = input('How many rounds do you like to play? ')
+    #     if number_of_rounds.isdigit() and int(number_of_rounds) > 0:
+    #         number_of_rounds = int(number_of_rounds)
+    #         break
+    #     else:
+    #         print('Invalid input. Please enter a positive number.')
     while True:
-        number_of_rounds = input('How many rounds do you like to play? ')
-        if number_of_rounds.isdigit() and int(number_of_rounds) > 0:
-            number_of_rounds = int(number_of_rounds)
-            break
-        else:
+        try:
+            number_of_rounds = int(input('How many rounds do you like to play? '))
+            if number_of_rounds > 0:
+                break
+            else:
+                print('Please enter a positive number.')
+        except ValueError:
             print('Invalid input. Please enter a positive number.')
+            continue
     user_player = HumanPlayer(number_of_rounds)
     computer_player = ComputerPlayer()
     game1 = Game(user_player, computer_player)
